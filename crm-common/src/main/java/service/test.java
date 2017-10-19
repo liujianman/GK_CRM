d82a865df208bb96166830d0f480bccb47e6ef67
mac_henry <henry525>
2017-10-19 13:11:02 +0800

    baselly initialize the project with adding the catalogue of controller，service and so on！！

---
+++ crm-common/src/main/java/service/test.java
@@ -0,0 +1,10 @@
+package service;
+
+/**
+ * @author tanjiancheng
+ * @email 1014216852@qq.com
+ * @git https://github.com/tanningmeng
+ * @date 17/10/19 13:00
+ */
+public class test {
+}
---
+++ crm-shiro/src/main/java/service/test.java
@@ -0,0 +1,10 @@
+package service;
+
+/**
+ * @author tanjiancheng
+ * @email 1014216852@qq.com
+ * @git https://github.com/tanningmeng
+ * @date 17/10/19 13:03
+ */
+public class test {
+}
